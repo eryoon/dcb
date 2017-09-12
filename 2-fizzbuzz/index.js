3f484f7c092149d30dca5bf47b3bc61ddf2ad509
Eric <eryoon2019@mirmanmustangs.org>
2017-09-12 08:16:54 -0700

    last changes

--- 2-fizzbuzz/index.js
+++ 2-fizzbuzz/index.js
@@ -40,7 +40,6 @@ function fizzBuzz(n) { //ELEGANT SOLUTION!
 
 
 //this is the worst code I've ever written...
-//DISGUSTING SOLUTION!
 function fbOneLine(n){return ((n % 15 == 0) ? "FizzBuzz!" : (n % 3 == 0) ? "Fizz!" : ((n % 5 == 0) ? "Buzz!" : n.toString()));}
 
 
